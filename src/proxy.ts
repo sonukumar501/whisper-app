6a4135278c57dc3a461f0171c3b89d2eabadec68
Sonu Kumar <sonukumar.official.200@gmail.com>
2026-09-23 10:33:31 +0530

    fix: rename middleware function to proxy for clarity

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const token = await getToken({ req: request });
 
   const url = request.nextUrl;
